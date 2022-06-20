f77aa3862468091267d967e93684b35aa4d9fd2a
juniri <ga243@naver.com>
2022-06-20 18:05:53 +0900

    0620 수업 끝

--- application/controllers/BoardController.php
+++ application/controllers/BoardController.php
@@ -1,7 +1,12 @@
 <?php
-
-  use application\controllers\Controller;
+  namespace application\controllers;
+  use application\models\BoardModel;
 
   class BoardController extends Controller{
-
+    public function list(){
+      $model = new BoardModel();
+      // $this->addAttribute("list", $model->selBoardList());
+      $this->list = $model->selBoardList(); //윗줄이랑 같음
+      return "board/list.php"; //view 파일명!!
+    }
   }
