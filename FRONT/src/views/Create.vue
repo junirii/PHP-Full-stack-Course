48fbce6ba47dad2b40ada9acad75f297322fe4fc
zosaekki <101864441+zosaekki@users.noreply.github.com>
2022-07-28 17:59:05 +0900

    TODO: insert img
    img preview, insert img

--- FRONT/src/views/Create.vue
+++ FRONT/src/views/Create.vue
@@ -1,43 +1,83 @@
 <template>
-        <div>
-            <span>글 제목</span>
-            <input type="text" placeholder="제목" v-model="board.title">
-        </div>
-        <div>
-            <span>지역</span>
-            <select v-model="board.iarea">
-                <option :key="iarea" v-for="(value, iarea) in areaList">{{ value.area_nm }}</option>
-            </select>
-            <select v-model="board.ilocation" v-if="showLocationSelect">
-                <option :key="ilocation" v-for="(value, ilocation) in locationList">{{ value.location_nm }}</option>
-            </select>
-        </div>
-        <div>
-            <span>성별</span>
-            <select v-model="board.gender">
-                <option value="0">혼성</option>
-                <option value="1">남</option>
-                <option value="2">여</option>
-            </select>
-        </div>
-        <div>
-            <span>인원수</span>
-            <input type="number" min="0" placeholder="인원수" v-model="board.people">
-        </div>
-        <div>
-            <span>연령대</span>
-            <select v-model="board.age">
-                <option value="1">10대</option>
-                <option value="2">20대</option>
-                <option value="3">30대</option>
-                <option value="4">40대</option>
-                <option value="5">50대</option>
-            </select>
+    <div>
+        <span>글 제목</span>
+        <input type="text" placeholder="제목" v-model="board.title">
+    </div>
+    <div>
+        <span>지역</span>
+        <select v-model="selectedArea" @change="showLocationOption()">
+            <option value="" selected>전체</option>
+            <option :key="item.iarea" :value="item.iarea" v-for="item in areaList">{{ item.area_nm }}</option>
+        </select>
+        <select v-model="selectedLocation" v-if="locationList.length > 1">
+            <option value="0" selected>전체</option>
+            <option :key="item.ilocation" :value="item.ilocation" v-for="item in locationList">{{ item.location_nm }}
+            </option>
+        </select>
+    </div>
+    <div>
+        <span>성별</span>
+        <select v-model="board.f_gender">
+            <option value="1">남</option>
+            <option value="2">여</option>
+            <option value="3">혼성</option>
+        </select>
+    </div>
+    <div>
+        <span>인원수</span>
+        <input type="number" min="0" placeholder="인원수" v-model="board.f_people">
+    </div>
+    <div>
+        <span>비용</span>
+        <select v-model="board.f_price">
+            <option value="1">0~5만원</option>
+            <option value="2">5~10만원</option>
+            <option value="3">10~20만원</option>
+            <option value="4">20~30만원</option>
+            <option value="5">30~40만원</option>
+            <option value="6">40~50만원</option>
+            <option value="7">50만원 이상</option>
+        </select>
+    </div>
+    <div>
+        <span>연령대</span>
+        <select v-model="board.f_age">
+            <option value="1">20~30대</option>
+            <option value="2">30~40대</option>
+            <option value="3">40~50대</option>
+            <option value="4">제한없음</option>
+        </select>
+    </div>
+    <div>
+        <span>날짜</span>
+        <input type="date" v-model="board.s_date"> ~ <input type="date" v-model="board.e_date">
+    </div>
+    <div>
+        <span>사진등록</span>
+        <img id="preview" @click="clickInputFile()" src="https://www.picng.com/upload/plus/png_plus_52132.png" width="160" height="160" style="cursor:pointer">
+        <div class="d-none">
+            <input id="inputImg" @change="readURL();" type="file" name="img" accept="image/*">
         </div>
 
-        <div>
-            <button type="button" class="btn btn-lg btn-danger" @click="boardInsert">저장</button>
+        <!-- <div>
+            <label for="file">사진 등록</label>
+            <input type="file" id="file" ref="files" @change="imageUpload" v-on:change="inputImgFile" />
         </div>
+        <div>
+            <span>이미지</span>
+            <div v-for="(file, index) in files" :key="index" class="file-preview-wrapper">
+                <div class="file-close-button" @click="imgDeleteButton" :name="file.number">
+                    X
+                </div>
+                <img :src="file.preview" />
+            </div>
+        </div> -->
+
+    </div>
+    <div>
+        <button type="button" class="btn btn-lg btn-danger" @click="boardInsert">저장</button>
+    </div>
+
 </template>
 
 <script>
@@ -45,20 +85,27 @@ export default {
     data() {
         return {
             board: {
-                title: '',
-                //gender: 0,
-                //people: 0,
-                //age: 0,
-                iarea: 0,
-                ilocation: 0,
                 iuser: 1,
-                s_date: "2020-02-02",
-                e_date: "2020-02-03",
-                main_img: "img"
+                title: '',
+                f_gender: 0,
+                f_price: 0,
+                f_people: 0,
+                f_age: 0,
+                area: 0,
+                location: 0,
+                s_date: '',
+                e_date: '',
+                main_img: ''
             },
             areaList: [],
             locationList: [],
-            showLocationSelect: true
+            showLocationSelect: false,
+            selectedArea: '',
+            selectedLocation: '',
+            // previewImage: '',
+            // files: [],
+            // filesPreview: [],
+            // uploadImageIndex: 0,
         }
     },
     created() {
@@ -70,16 +117,76 @@ export default {
             this.areaList = await this.$get('/board/areaList', {});
             console.log(this.areaList);
         },
-        async getLocationList() {
-            this.locationList = await this.$get('/board/locationList', {});
+        async getLocationList(iarea) {
+            this.locationList = await this.$get(`/board/locationList/${iarea}`, {});
             console.log(this.locationList);
         },
+        showLocationOption() {
+            this.selectedLocation = '';
+            this.locationList = [];
+            this.getLocationList(this.selectedArea);
+        },
         async boardInsert() {
-            const result = this.$post('/board/boardInsert', this.board);
+            const inputFile = document.querySelector('#file');
+            console.log(inputFile.files[0].name);
+            this.board.main_img = inputFile.files[0].name;
+            this.board.area = this.selectedArea;
+            if (this.selectedLocation) {
+                this.board.location = this.selectedLocation;
+            } else {
+                this.board.location = 0;
+            }
+            const result = this.$post('/board/create', this.board);
             console.log(result);
+            this.$swal.fire('글작성 성공!', '', 'success');
+        },
+        clickInputFile() {
+            const preview = document.querySelector('#preview');
+            const input = document.querySelector('#inputImg');
+            preview.addEventListener('click', () => {
+                input.click();
+            })
+        },
+        readURL() {
+            if (this.files && this.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    document.getElementById('preview').src = this.target.result;
+                };
+                reader.readAsDataURL(input.files[0]);
+            } else {
+                document.getElementById('preview').src = "";
+            }
         }
+
+        
+        // imageUpload() {
+        //     console.log(this.$refs.files.files);
+        //     let num = -1;
+        //     for (let i = 0; i < this.$refs.files.files.length; i++) {
+        //         this.files = [
+        //             ...this.files,
+        //             //이미지 업로드
+        //             {
+        //                 //실제 파일
+        //                 file: this.$refs.files.files[i],
+        //                 //이미지 프리뷰
+        //                 preview: URL.createObjectURL(this.$refs.files.files[i]),
+        //                 //삭제및 관리를 위한 number
+        //                 number: i
+        //             }
+        //         ];
+        //         num = i;
+        //     }
+        //     this.uploadImageIndex = num + 1;
+        //     console.log(this.files);
+        // },
+        // inputImgFile() {
+        //     this.board.main_img = this.$refs.files.name;
+        // },
     }
 }
+
 </script>
 
 <style>
--- application/models/BoardModel.php
+++ application/models/BoardModel.php
@@ -1,42 +1,80 @@
 <?php
+
 namespace application\models;
+
 use PDO;
 
-class BoardModel extends Model {
+class BoardModel extends Model
+{
     // 리스트 
-    public function boardList() {
-        $sql="SELECT * 
-        FROM t_board A
-        INNER JOIN t_user B
-        ON A.iuser = B.iuser";
+    public function boardList()
+    {
+        $sql = "SELECT * 
+            FROM t_board A
+            INNER JOIN t_user B
+            ON A.iuser = B.iuser";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
-    
-    public function boardInsert(&$param) {
+
+    // area list
+    public function areaList()
+    {
+        $sql = "SELECT * FROM t_area";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // location list
+    public function locationList(&$param)
+    {
+        $sql = "SELECT * FROM t_area A
+        LEFT JOIN t_location B
+        ON A.iarea = B.iarea
+        WHERE A.iarea = :iarea";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(":iarea", $param["iarea"]);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function boardInsert(&$param)
+    {
         $sql = "INSERT INTO t_board
                 SET iuser = :iuser
                   , title = :title
-                  , ctnt = :ctnt
                   , area = :area
                   , main_img = :main_img
                   , s_date = :s_date
-                  , e_date = :e_date";
+                  , e_date = :e_date
+                  , f_people = :f_people
+                  , f_price = :f_price
+                  , f_gender = :f_gender
+                  , f_age = :f_age";
+        if(isset($param["location"])) {
+            $sql .= ", location = :location";
+        }
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(":iuser", $param["iuser"]);
         $stmt->bindValue(":title", $param["title"]);
-        $stmt->bindValue(":ctnt", $param["ctnt"]);
         $stmt->bindValue(":area", $param["area"]);
+        $stmt->bindValue(":location", $param["location"]);
         $stmt->bindValue(":main_img", $param["main_img"]);
         $stmt->bindValue(":s_date", $param["s_date"]);
         $stmt->bindValue(":e_date", $param["e_date"]);
+        $stmt->bindValue(":f_people", $param["f_people"]);
+        $stmt->bindValue(":f_price", $param["f_price"]);
+        $stmt->bindValue(":f_gender", $param["f_gender"]);
+        $stmt->bindValue(":f_age", $param["f_age"]);
         $stmt->execute();
         return intval($this->pdo->lastInsertId());
     }
 
-    public function detail(&$param) {
-        $sql="SELECT *
+    public function detail(&$param)
+    {
+        $sql = "SELECT *
         FROM t_board A
         INNER JOIN t_board_ctnt B
         ON A.iboard = B.iboard
