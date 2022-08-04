24c6da40a81e77464e39b382dd0d751de78917c7
junirii <ga243@naver.com>
2022-08-04 10:08:49 +0900

    first commit

---
+++ vue-movie-pjt/src/views/BoxOfficeByDay.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>BoxOfficeByDay</h1>
+    <div>
+      <input type="date" v-model="selectedDate">
+      <button @click="search">검색</button>
+    </div>
+    <rank-table :list="list" />
+  </div>
+</template>
+
+<script>
+import RankTable from '../components/boxoffice/RankTable';
+
+export default{
+  components: {
+    RankTable
+  },
+  data(){
+    return {
+      selectedDate: '',
+      list: []
+    }
+  },
+  methods: {
+    search(){
+      const targetDt = this.selectedDate.replaceAll('-', '');
+      this.getData(targetDt);
+    },
+    async getData(targetDt) {
+        const data = await this.getBoxOfficeByDay(targetDt);
+        this.list = data.boxOfficeResult.dailyBoxOfficeList;
+    }
+  },
+  created(){
+    const d = new Date();
+    d.setDate(d.getDate() - 1);
+    this.selectedDate = this.getOnlyDateStr(d);
+  }
+}
+</script>
---
+++ vue-movie-pjt/src/views/BoxOfficeByWeek.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+    <h1>{{title}}</h1>
+    <div>
+      <input type="date" v-model="selectedDate">
+      <button @click="search">검색</button>
+    </div>
+    <rank-table :list="list" />
+  </div>
+</template>
+
+<script>
+import RankTable from '../components/boxoffice/RankTable';
+
+export default {
+  components: {
+    RankTable
+  },
+  data(){
+    return {
+      title: '주간 박스오피스',
+      selectedDate: '',
+      list: []
+    }
+  },
+  methods: {
+    search(){
+      const targetDt = this.selectedDate.replaceAll('-', '');
+      this.getData(targetDt);
+    },
+    async getData(targetDt) {
+        const data = await this.getBoxOfficeByWeek(targetDt, '0');
+        this.list = data.boxOfficeResult.weeklyBoxOfficeList;
+    }
+  },
+  created(){
+    const d = new Date();
+    d.setDate(d.getDate() - 1);
+    this.selectedDate = this.getOnlyDateStr(d);
+  }
+}
+</script>
+
+<style>
+
+</style>
