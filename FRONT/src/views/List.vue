e39f34322e2a11f6ee78159fee06762a2232dfb9
zosaekki <101864441+zosaekki@users.noreply.github.com>
2022-08-06 12:06:35 +0900

    list filter button
    list filter button

--- FRONT/src/views/List.vue
+++ FRONT/src/views/List.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="list mt-3">
     <div class="container">   
+      <button type="button" @click="showFilterModal">필터</button>
+      <FilterModal :show="modalShow" @close="hiddenModal"></FilterModal>
       <div class="row">
         <div class="col-xl-3 col-lg-4 col-md-6" style="padding: 25px 25px 25px;"
             :key="item.itravel" v-for="item in list">
@@ -32,70 +34,80 @@
 </template>
 
 <script>
+import FilterModal from '@/components/common/FilterModal.vue';
 
 export default {
-  data() {
-    return{
-    list: [],
-    travelFavList: [],
-    favItravelList: [],
-    iuser: null
-    }
-  },
-  methods: {
-    async travelList() {
-      console.log(this.$store.state.filter);
-      const filter = this.$store.state.filter;
-      console.log(filter);
-      this.list = await this.$post('/travel/travelList', {
-      filter: this.$store.state.filter
-      });
+    data() {
+        return {
+            list: [],
+            travelFavList: [],
+            favItravelList: [],
+            iuser: null,
+            modalShow: false
+        };
     },
-    async goToDetail(itravelNum) {
-      this.$store.state.itravel = itravelNum;
-      this.$router.push({name: 'detail'});
-    },
-    async favItravel() {
-      this.iuser = this.$store.state.user.iuser;
-      this.travelFavList = await this.$get(`/travel/travelFav/${this.iuser}`, {});
-      this.travelFavList.result.forEach(item => {
-        this.favItravelList.push(item.itravel);
-      });
-    },
-    heartColor(itravel) {
-      if(this.favItravelList.includes(itravel)){
-        return true;
-      }else{
-        return false;
-      }
-    },
-    async good(event, itravel) {
-      if(event.target.classList.contains('fa-regular')){ //검은 하트일 때
-        const res = await this.$post(`/travel/travelFav/${this.iuser}/${itravel}`);
-        if(res.result === 1){ // 좋아요 성공 시
-          event.target.classList.remove('fa-regular');
-          event.target.classList.add('fa-solid');
-          event.target.style.color = "red";
+    methods: {
+        showFilterModal() {
+          this.modalShow = true;
+        },
+        hiddenModal() {
+          this.modalShow = false;
+        },
+        async travelList() {
+            console.log(this.$store.state.filter);
+            const filter = this.$store.state.filter;
+            console.log(filter);
+            this.list = await this.$post("/travel/travelList", {
+                filter: this.$store.state.filter
+            });
+        },
+        async goToDetail(itravelNum) {
+            this.$store.state.itravel = itravelNum;
+            this.$router.push({ name: "detail" });
+        },
+        async favItravel() {
+            this.iuser = this.$store.state.user.iuser;
+            this.travelFavList = await this.$get(`/travel/travelFav/${this.iuser}`, {});
+            this.travelFavList.result.forEach(item => {
+                this.favItravelList.push(item.itravel);
+            });
+        },
+        heartColor(itravel) {
+            if (this.favItravelList.includes(itravel)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+        async good(event, itravel) {
+            if (event.target.classList.contains("fa-regular")) { //검은 하트일 때
+                const res = await this.$post(`/travel/travelFav/${this.iuser}/${itravel}`);
+                if (res.result === 1) { // 좋아요 성공 시
+                    event.target.classList.remove("fa-regular");
+                    event.target.classList.add("fa-solid");
+                    event.target.style.color = "red";
+                }
+            }
+            else if (event.target.classList.contains("fa-solid")) {
+                const res = await this.$delete(`/travel/travelFav/${this.iuser}/${itravel}`);
+                if (res.result === 1) { // 좋아요 취소 성공 시
+                    event.target.classList.add("fa-regular");
+                    event.target.classList.remove("fa-solid");
+                    event.target.style.color = "";
+                }
+            }
+        },
+        async goToMyPage(iuserNum) {
+            this.$store.state.feedIuser = iuserNum;
+            this.$router.push({ name: "mypage" });
         }
-      }else if(event.target.classList.contains('fa-solid')){
-        const res = await this.$delete(`/travel/travelFav/${this.iuser}/${itravel}`);
-        if(res.result === 1){ // 좋아요 취소 성공 시
-          event.target.classList.add('fa-regular');
-          event.target.classList.remove('fa-solid');
-          event.target.style.color = "";
-        }
-      }
     },
-    async goToMyPage(iuserNum) {
-      this.$store.state.feedIuser = iuserNum;
-      this.$router.push({name: 'mypage'});
-    }
-  },
-
-  created() {
-    this.travelList();
-    this.favItravel();
-  },
+    created() {
+        this.travelList();
+        this.favItravel();
+    },
+    components: { FilterModal }
 }
 
 </script>
