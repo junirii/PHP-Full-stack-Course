ce613bee0162170fffbbbe79f3bf594401339aec
junirii <105265611+junirii@users.noreply.github.com>
2022-08-08 17:09:01 +0900

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,69 @@
+const socket = io();
+
+let name = prompt('당신의 이름은?', '');
+let uid;
+socket.on('connect', function() {
+  if(name === '') {
+    name = '익명';
+  }
+
+  const divMe = document.querySelector('#me');
+  divMe.appendChild(document.createElement('div')).innerText = `${name} (나)`;
+  socket.emit('newUser', name);
+});
+
+socket.on('newUser', function(userObj){
+  uid = userObj.uid;
+})
+
+socket.on('users', function(userList){
+  const divOthers = document.querySelector('#others');
+  divOthers.innerHTML = '';
+
+  userList.forEach(function(item){
+    if(uid !== item.uid){
+      const divOther = divOthers.appendChild(document.createElement('div'));
+      divOther.innerText = `${item.name} 님`;
+    }
+  })
+})
+
+socket.on('update', function(data) {
+  const chat = document.getElementById('chat');
+  const message = chat.appendChild(document.createElement('div'));
+  message.innerText = `${data.name}: ${data.message}`;
+  
+  let className = '';
+  switch(data.type) {
+    case 'message':
+      className = 'other';
+      break;
+    case 'connect':
+      className = 'connect';
+      break;
+    case 'disconnect':
+      className = 'disconnect';
+      break;
+  }
+  message.classList.add(className);
+  chat.scrollTop = chat.scrollHeight;
+});
+
+function send() {
+  const message = document.getElementById('test').value;
+  const chat = document.getElementById('chat');
+  const msg = chat.appendChild(document.createElement('div'));
+
+  document.getElementById('test').value = '';
+  msg.innerText = message;
+  msg.classList.add('me');
+
+  socket.emit('message', {type: 'message', message: message});
+  chat.scrollTop = chat.scrollHeight;
+}
+
+function enter(e){
+    if(e.keyCode === 13){
+      send();
+    }
+}
