0084dfcde34385320e45ee42d3dc97f289827895
COOL_CAL <78495826+COOL-CAL@users.noreply.github.com>
2022-08-09 13:13:40 +0900

    오전 css 수정

--- FRONT/src/views/Home.vue
+++ FRONT/src/views/Home.vue
@@ -3,11 +3,11 @@
     <!-- <div class="background">
       <p>가보자고</p>
     </div> -->
-    <img class="bg" src="../../../static/img_used/main.png" alt="">
+    <!-- <img class="bg" src="../../../static/img_used/main.png" alt=""> -->
     <div class="setting-btn">
-      <button class="btn" type="button" v-if="this.$store.state.isLogin" @click="loginCheck('Create')">HOSTING</button>
-      <button class="btn" type="button" v-if="this.$store.state.isLogin" @click="loginCheck('SelectPage')">SEARCH</button>
-      <button class="btn" type="button" v-if="!this.$store.state.isLogin" @click="showModal">JOIN</button>
+      <button class="main-btn" type="button" v-if="this.$store.state.isLogin" @click="loginCheck('Create')">여행 만들기</button>
+      <button class="main-btn" type="button" v-if="this.$store.state.isLogin" @click="loginCheck('SelectPage')">여행 찾기</button>
+      <button class="main-btn" type="button" v-if="!this.$store.state.isLogin" @click="showModal">회원가입</button>
     </div>
     <LoginModal 
     :show="modalShow" 
@@ -58,30 +58,50 @@ export default {
 <style scoped>
   body {
     z-index: auto;
-    height: 100%;
+    height: 100vh;
+    width: 100vw;
+    background-repeat : no-repeat;
+    background-size : cover;
+    background-image: url('../../../static/img_used/main.png');
+    background-position-y: -100px;
+    /* transition: 3s; */
   }
-  .background {
+  /* .background {
     background-image: url('../../../static/img_used/main.png');
     background-size: cover;
-    /* background-clip: text;
+    background-clip: text;
     -webkit-background-clip: text;
-    color: transparent; */
-  }
-  /* .background p {
-    font-size: 300px;
-    font-weight: bolder;
-    text-align: center;
+    color: transparent;
   } */
-  .bg {
-    width: 1903px;
-    height: 1000px;
+  /* .bg {
     position: relative;
-    background-size: contain;
-  }
+    top: 100px;
+  } */
   .setting-btn {
-    position: relative;
-    bottom: 400px;
+    /* z-index: 9999; */
+    text-align: center;
+    position: absolute;
+    bottom: 300px;
+    left: 800px;
   }
+  .main-btn { 
+    font-family: 'LeferiPoint-WhiteA';
+    display: inline-block;
+    text-decoration: none;
+    background: #fff;
+    padding: 8px 30px;
+    border-radius: 7px;
+    /* font-family: helvetica; */
+    font-weight: bold;
+    color: #2d7ac2;
+    box-shadow: 0 8px 0 #b1b1b1;
+    transition: all 0.2s;
+}
+
+.main-btn:active {
+    transform: translateY(4px);
+    box-shadow: 0 4px 0 #2d7ac2;
+}
   button {
     margin: 15px;
   }
